91e68776213f3c4f61b62cbd42f9f1cbf8e7b1ad
ByeongSe0k <pojdlqudtjr@soongsil.ac.kr>
2024-01-02 14:47:31 +0900

    LED combined

--- sonar.py
+++ sonar.py
@@ -1,3 +1,20 @@
 from RobokitRS.RobokitRS import *
 import time as t
+
 rs = RobokitRS()
+
+rs.port_open("COM5")
+
+#set led red, led on
+rs.set_rgb_led_red(7)
+rs.set_rgb_led_on(7)
+
+rs.sonar_begin(13)
+
+while 1:
+    SONAR = rs.sonar_read(13)
+    t.sleep(0.2)
+    if(SONAR < 20):
+        break
+rs.set_rgb_led_off(7)
+
