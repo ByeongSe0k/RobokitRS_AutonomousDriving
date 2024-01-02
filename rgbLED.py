e56ad01b6428b99edc0c85234d04c5a567efd992
ByeongSe0k <pojdlqudtjr@soongsil.ac.kr>
2024-01-02 13:51:20 +0900

    led finished

--- rgbLED.py
+++ rgbLED.py
@@ -1,10 +1,32 @@
 from RobokitRS.RobokitRS import *
 import time as t
-
 rs = RobokitRS()
 
 rs.port_open("COM5")
 
 #RGB LED set Color
-rs.set_rgb_led_color(12,100,100,100)
+rs.set_rgb_led_color(7,100,100,100)
 rs.set_rgb_led_on(7)
+t.sleep(1)
+rs.set_rgb_led_off(7)
+
+colors = ['red', 'navy', 'random']
+
+for color in colors:
+    if color == 'red':
+        rs.set_rgb_led_red(7)
+        rs.set_rgb_led_on(7)
+
+    elif color == 'navy':
+        rs.set_rgb_led_navy(7)
+        rs.set_rgb_led_on(7)
+
+    elif color == 'random':
+        rs.set_rgb_led_random(7)
+        rs.set_rgb_led_on(7)
+
+    else:
+        rs.set_rgb_led_off(7)
+
+t.sleep(1)
+rs.set_rgb_led_off(7)
