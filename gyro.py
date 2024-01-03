625f9e8b106a1f6617190d1e0b4667a5f21db9eb
ByeongSe0k <pojdlqudtjr@soongsil.ac.kr>
2024-01-04 06:47:04 +0900

    gyro modefied

--- gyro.py
+++ gyro.py
@@ -2,23 +2,29 @@ from RobokitRS.RobokitRS import *
 import time as t
 
 rs = RobokitRS()
-
 rs.port_open("COM5")
 
-rs.gyro_begin()
-rs.set_gyro_default_position(0)
+def gyro():
+    rs.gyro_begin()
+    rs.set_gyro_default_position(0)
 
+    while(1):
+        angleX = rs.gyro_read(GyroDataType.ANGLE_X)
+        angleY = rs.gyro_read(GyroDataType.ANGLE_Y)
+        angleZ = rs.gyro_read(GyroDataType.ANGLE_Z)
+        gyroX = rs.gyro_read(GyroDataType.GYRO_X)
+        gyroY = rs.gyro_read(GyroDataType.GYRO_Y)
+        gyroZ = rs.gyro_read(GyroDataType.GYRO_Z)
+        shake = rs.gyro_read(GyroDataType.SHAKE)
+        t.sleep(0.02)
 
-while(1):
-    angleX = rs.gyro_read(GyroDataType.ANGLE_X)
-    angleY = rs.gyro_read(GyroDataType.ANGLE_Y)
-    angleZ = rs.gyro_read(GyroDataType.ANGLE_Z)
-    gyroX = rs.gyro_read(GyroDataType.GYRO_X)
-    gyroY = rs.gyro_read(GyroDataType.GYRO_Y)
-    gyroZ = rs.gyro_read(GyroDataType.GYRO_Z)
-    shake = rs.gyro_read(GyroDataType.SHAKE)
-    t.sleep(0.02)
-
+        print("(",angleX, angleY, angleZ,")","(",gyroX, gyroY, gyroZ,")","(",shake,")")
     
 
-    print("(",angleX, angleY, angleZ,")","(",gyroX, gyroY, gyroZ,")","(",shake,")")
+def main():
+    gyro()
+    
+
+
+if (__name__=="__main__"):
+    main()
